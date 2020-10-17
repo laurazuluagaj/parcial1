91e757de8a3f23244aac0cadb7e01858c850da22
laura.zuluagaj <laura.zuluagaj@udea.edu.co>
2020-10-17 17:08:26 -0500

    Modificacion menu

--- Solucion/main.cpp
+++ Solucion/main.cpp
@@ -18,15 +18,16 @@ int main()
     //Calculamos los radios de destruccion de la defensa y el cliente
     RO=0.05*d;
     RD=0.025*d;
+    cout << "1-> Generar disparos ofensivos que comprometan la integridad del canon defensivo." << endl;
+    cout << "2-> Generar disparos defensivos que comprometan la integridad del canon ofensivo." << endl;
+    cout << "3-> Dado un disparo ofensivo, generar disparos defensivos que impida que el canon defensivo sea destruido sin importar si el canon ofensivo pueda ser destruido." << endl;
+    cout << "4-> Dado un disparo ofensivo, generar disparo defensivos que impidan que los canones defensivo y ofensivo puedan ser destruidos." << endl;
     cout << "Ingrese una de las siguientes opciones: ";
-    cout << "1-> Generar disparos ofensivos que comprometan la integridad del cañón defensivo." << endl;
-    cout << "2-> Generar disparos defensivos que comprometan la integridad del cañón ofensivo." << endl;
-    cout << "3-> Dado un disparo ofensivo, generar disparos defensivos que impida que el cañón defensivo sea destruido sin importar si el cañón ofensivo pueda ser destruido." << endl;
-    cout << "4-> Dado un disparo ofensivo, generar disparo defensivos que impidan que los cañones defensivo y ofensivo puedan ser destruidos." << endl;
     cin >> opcion;
     switch (opcion) {
         case 1:{
-            float AngO, Vel1[2]={}, Vel2[2]={}; //Vel1: Velocidad cañon en x,y del enemigo
+            float AngO, Vel1[2]={}, Vel2[2]={}; //Vel1: Velocidad canon en x,y del enemigo
+            unsigned int t1;
             cout << "Ingrese la posicion en x que tiene el canon del enemigo: ";
             cin >> PosicionO[0];
             cout << "Ingrese la velocidad que tiene el enemigo: ";
@@ -38,6 +39,8 @@ int main()
             cin >> AngO;
             Vel1[0]=VelO*(cos(AngO));
             d-=PosicionO[0];
+            d=(d-(d-PosicionD[0]));
+            t1=(d-PosicionO[0])/Vel1[0];
             }
         break;
     }
